7686b282abbdbabdcfe3c86480c38135f829be7f
Mingqian Ye <mingqian.ye@33across.com>
2016-11-01 14:46:03 -0700

    work

--- app/services/survey_store_service.rb
+++ app/services/survey_store_service.rb
@@ -8,10 +8,10 @@ class SurveyStoreService
       survey = Survey.find_by_id(@hash['uuid']) || Survey.create
       survey.update_attributes(title: @hash['title'])
       @hash['questions'].map do |q|
-        question = Question.find_by_id(@hash['uuid']) || Question.create
-        question.update_attributes(text: @hash['text'])
+        question = Question.find_by_id(q['uuid']) || Question.create
+        question.update_attributes(text: q['text'])
         q['answers'].each do |a|
-          answer = Answer.find_by_id(@hash['uuid']) || Answer.create
+          answer = Answer.find_by_id(a['uuid']) || Answer.create
           answer.update_attributes(text: a['text'])
           question.answers << answer
         end
